11dd631da2f58a2625686795fa3cb92836fc923b
Youssef-ashraf-Ramadan <youssefashrafr@hotmail.com>
2024-02-03 16:26:02 +0200

    third-commit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let navMenu = document.querySelector(".nav-menu");
 let closeMenuBtn = document.querySelector(".close-menu");
 let opanMenuBtn = document.querySelector(".menu-icon");
+let links = document.querySelectorAll(".head .container .links ul li a");
+let sections = document.querySelectorAll(".home, .categories, .menu");
 
 // Open Menu
 opanMenuBtn.addEventListener("click", () => {
@@ -11,3 +13,27 @@ opanMenuBtn.addEventListener("click", () => {
 closeMenuBtn.addEventListener("click", () => {
   navMenu.classList.remove("active-menu");
 });
+
+// Active Link On Click
+links.forEach((ele) => {
+  ele.addEventListener("click", (e) => {
+    links.forEach((ele) => {
+      ele.classList.remove("active");
+    });
+    e.currentTarget.classList.add("active");
+  });
+});
+
+// Active Link On Scroll
+window.addEventListener("scroll", function () {
+  sections.forEach(function (s) {
+    if (window.scrollY >= s.offsetTop - 200) {
+      links.forEach((e) => {
+        e.classList.remove("active");
+        `#${s.id}` == e.getAttribute("href") ? e.classList.add("active") : "";
+      });
+    }
+  });
+});
+
+console.log(Array.from(links));
